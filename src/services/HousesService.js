b3aed3f4732566e8b64aee96f525f0dd373995a6
heather bickish <hbickish@gmail.com>
2024-12-04 13:51:44 -0700

    saving houses to appstate

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,10 +1,15 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { House } from "@/models/House.js"
+import { AppState } from "@/AppState.js"
 
 class HousesService {
   async getHouses() {
     const response = await api.get('api/houses')
     logger.log('Got 🏠🏠🏠', response.data)
+    const houses = response.data.map(housePojo => new House(housePojo))
+    AppState.houses = houses
+    logger.log(houses)
 
   }
 
